8b512282503433ec15481c150ef817e59052f5fd
Alberto Hernandez <alberto.hernandez@softel.mx>
2020-06-02 14:47:37 -0500

    Ble commands class was added

---
+++ cir_wireless/bluetooth_low_energy/BleCommands.swift
@@ -0,0 +1,14 @@
+//
+//  BleCommands.swift
+//  cir_wireless
+//
+//  Created by softel on 02/06/20.
+//  Copyright © 2020 SOFTEL. All rights reserved.
+//
+
+import Foundation
+
+// This class contains all ble commands used for programming CIR 2017 and Wireless
+class BleCommands {
+    
+}
